e0402bb0f057cc244659b360c881f4d049b2c8d3
Amr Gawish <amr-gawish@outlook.com>
2022-10-30 13:59:47 +0200

    add sass

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import "./App.css";
+import "./App.scss";
 import Button from "react-bootstrap/Button";
+import Navbar from "./components/navbar/Navbar";
 function App() {
   return (
     <div className="App">
       <>
-        <h1>goooo</h1>
-        <Button variant="primary">Primary</Button>{" "}
+        <Navbar />
       </>
     </div>
   );
