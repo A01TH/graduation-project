8dda95ae2c3cf0a26aab67df08b4c090ef5b40e9
Mostafa Khafaji <mostafakhafaji2050@gmail.com>
2022-10-31 14:22:21 +0200

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.scss";
 import Navbar from "./components/navbar/Navbar";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import GetStarted from "./Pages/GetStarted/GetStarted";
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <GetStarted />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<GetStarted />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,29 +1,81 @@
 import React, { useState } from "react";
+import { Dropdown } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../assets/logo/Logo.png";
+import user from "../../assets/navbar/nav-user.png";
 import "./Navbar.scss";
 const Header = () => {
-  const [logged, setLogged] = useState(false);
+  const [logged, setLogged] = useState(true);
+  const [mobNav, setMobNav] = useState(false);
+  const handleMobileNav = () => {
+    setMobNav((prev) => !prev);
+  };
   return (
-    <div className="navbar">
-      <header>
-        <div className="logo">
-          <h1>LOGO</h1>
-        </div>
-        {logged ? (
-          <nav>
-            <ul>
-              <li>Home</li>
-              <li>Categories</li>
-              <li>Messages</li>
-              <li>Profile</li>
-            </ul>
-          </nav>
-        ) : (
-          <div className="nav-button">
-            <button>Get Started</button>
+    <section className="nav">
+      <div className="container">
+        <header className="d-flex align-items-center justify-content-between w-100 h-100">
+          <div className="brand">
+            <Link to="/">
+              <img src={logo} alt="" />
+            </Link>
           </div>
-        )}
-      </header>
-    </div>
+          <nav>
+            <div className="nav-mobile ">
+              <div
+                id="nav-toggle"
+                onClick={handleMobileNav}
+                className={mobNav && "active"}
+              >
+                <span></span>
+              </div>
+            </div>
+            {logged ? (
+              <ul
+                className={`nav-list list-unstyled  d-flex  d-flex gap-4 align-items-center justify-content-center ${
+                  mobNav && "show"
+                }`}
+              >
+                <li className="link">
+                  <NavLink to="/home">Home</NavLink>
+                </li>
+                <li className="link">
+                  <NavLink to="/home">Categories</NavLink>
+                </li>
+                <li className="link">
+                  <NavLink to="/home">Messages</NavLink>
+                </li>
+                <li>
+                  <Dropdown>
+                    <Dropdown.Toggle
+                      variant="transparent"
+                      id="dropdown-basic"
+                      className="border-0"
+                    >
+                      <img src={user} alt="user" />
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu className="bg-transparent">
+                      <Dropdown.Item href="#/action-1">Profile</Dropdown.Item>
+                      <Dropdown.Item href="#/action-2">Settings</Dropdown.Item>
+                      <Dropdown.Item href="#/action-3">Connects</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </li>
+              </ul>
+            ) : (
+              <ul className="nav-list list-unstyled  d-flex gap-4  align-items-center justify-content-center">
+                <li className="link">
+                  <NavLink to="/home">Why Chall.go</NavLink>
+                </li>
+                <li>
+                  <button className="btn btn-light">Get Started</button>
+                </li>
+              </ul>
+            )}
+          </nav>
+        </header>
+      </div>
+    </section>
   );
 };
 
