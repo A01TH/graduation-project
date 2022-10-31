41c932953cc0b72e6c8c6ebdbd759fdaba72d2d0
Amr Gawish <amr-gawish@outlook.com>
2022-10-31 14:25:06 +0200

    feat login update 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.scss";
+import Login from "./components/auth/Login";
 import Navbar from "./components/navbar/Navbar";
 import GetStarted from "./Pages/GetStarted/GetStarted";
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <GetStarted />
+      <Login />
+      {/* <Navbar />
+      <GetStarted /> */}
     </div>
   );
 }
