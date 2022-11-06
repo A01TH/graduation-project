b5c77c71f137e2b8447e4781631cd6e8a3381341
Mostafa Khafaji <mostafakhafaji2050@gmail.com>
2022-11-07 01:56:04 +0200

    added loader for posts

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -16,38 +16,40 @@ const Home = () => {
   const { challengeCollection, userCollection } = useContext(FirebaseContext);
   const [challenges] = useCollectionData(challengeCollection);
   const { currentUser } = useContext(currentContext);
-  const [currentUserPosts, setcurrentUserPosts] = useState();
+  const [currentUserPosts, setcurrentUserPosts] = useState(challenges);
 
+  // const update = () => {
+  //   // EDIT DATA
+  //   userCollection
+  //     .doc(currentUser[0].uid)
+  //     .set(
+  //       {
+  //         name: "Mostafa Khafaji",
+  //         photoUrl:
+  //           "https://lh3.googleusercontent.com/a/ALm5wu3n5EcjNxxHrNesIcq8ZwxrXXiXaBA0q2xZXJ2J_g=s96-c",
+  //         points: 100,
+  //       },
+  //       { merge: true }
+  //     )
+  //     .then(() => {
+  //       toast("Your Name Has Updated Successfully", {
+  //         position: "top-center",
+  //         autoClose: 2000,
+  //         hideProgressBar: false,
+  //         closeOnClick: true,
+  //         pauseOnHover: true,
+  //         draggable: true,
+  //         progress: undefined,
+  //         theme: "dark",
+  //       });
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error writing document: ", error);
+  //     });
+  // };
   const update = () => {
-    // EDIT DATA
-    userCollection
-      .doc(currentUser[0].uid)
-      .set(
-        {
-          name: "Mostafa Khafaji",
-          photoUrl:
-            "https://lh3.googleusercontent.com/a/ALm5wu3n5EcjNxxHrNesIcq8ZwxrXXiXaBA0q2xZXJ2J_g=s96-c",
-          points: 100,
-        },
-        { merge: true }
-      )
-      .then(() => {
-        toast("Your Name Has Updated Successfully", {
-          position: "top-center",
-          autoClose: 2000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "dark",
-        });
-      })
-      .catch((error) => {
-        console.error("Error writing document: ", error);
-      });
+    console.log(currentUserPosts);
   };
-
   useEffect(() => {
     if (currentUser) {
       const filteredChallenges = challenges?.filter((challenge) => {
@@ -63,10 +65,10 @@ const Home = () => {
         <div className="row align-items-start justify-content-between">
           <div className="col-md-6 col-sm-12 mb-5 offset-1">
             <Post />
-            {challenges ? (
+            {/* {currentUserPosts ? (
               <>
-                {challenges?.length > 0 ? (
-                  challenges?.map((post, index) => {
+                {currentUserPosts?.length > 0 ? (
+                  currentUserPosts?.map((post, index) => {
                     return <ChallengeCard post={post} key={index} />;
                   })
                 ) : (
@@ -87,7 +89,32 @@ const Home = () => {
               </>
             ) : (
               <h1>No Posts Yet</h1>
+            )} */}
+            {currentUserPosts ? (
+              <>
+                {currentUserPosts?.map((post, index) => {
+                  return <ChallengeCard post={post} key={index} />;
+                })}
+              </>
+            ) : (
+              <ContentLoader
+                speed={2}
+                width={400}
+                height={460}
+                viewBox="0 0 400 460"
+                backgroundColor="#f3f3f3"
+                foregroundColor="#ecebeb"
+              >
+                <circle cx="31" cy="31" r="15" />
+                <rect x="58" y="18" rx="2" ry="2" width="140" height="10" />
+                <rect x="58" y="34" rx="2" ry="2" width="140" height="10" />
+                <rect x="0" y="60" rx="2" ry="2" width="400" height="400" />
+              </ContentLoader>
             )}
+
+            {currentUserPosts?.length === 0 ? (
+              <h1 className="text-light">No Posts</h1>
+            ) : null}
           </div>
           <div className="d-none d-md-block col-3">
             <RecommendedChallengers />
