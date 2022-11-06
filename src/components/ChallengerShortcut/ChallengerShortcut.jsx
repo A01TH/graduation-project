e1fc39de767058d95fcd0935a6a5eb3586dbcdaa
Amr Gawish <amr-gawish@outlook.com>
2022-11-06 22:38:21 +0200

    css editing

--- src/components/ChallengerShortcut/ChallengerShortcut.jsx
+++ src/components/ChallengerShortcut/ChallengerShortcut.jsx
@@ -3,9 +3,9 @@ import { FaUserPlus } from "react-icons/fa";
 
 const ChallengerShortcut = () => {
   return (
-    <div className="d-flex justify-content-between border-top p-2 align-items-center">
+    <div className=" d-flex justify-content-between  p-2 align-items-center bg-c-grey my-2 mx-1 rounded-2 bg-c-grey-hover">
       <div className="d-flex">
-        <div className="me-2 rounded-5 overflow-hidden">
+        <div className="me-2 rounded-5 overflow-hidden ">
           <img src="https://via.placeholder.com/50" className="w-100" />
         </div>
         <div>
@@ -15,7 +15,7 @@ const ChallengerShortcut = () => {
       </div>
       <div>
         <button className="btn">
-          <FaUserPlus className="fs-2" />
+          <FaUserPlus className="fs-2 text-grey-lite text-grey-lite-hover" />
         </button>
       </div>
     </div>
