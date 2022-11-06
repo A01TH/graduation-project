de669ddd3c808567e49c84f37edf4d68a8ada87a
Amr Gawish <amr-gawish@outlook.com>
2022-11-07 00:06:25 +0200

    edit home stlying v0.04

--- src/components/ChallengerShortcut/ChallengerShortcut.jsx
+++ src/components/ChallengerShortcut/ChallengerShortcut.jsx
@@ -3,18 +3,21 @@ import { FaUserPlus } from "react-icons/fa";
 
 const ChallengerShortcut = ({ name, photoURL }) => {
   return (
-    <div className=" d-flex justify-content-between  p-2 align-items-center bg-c-grey my-2 mx-1 rounded-2 bg-c-grey-hover">
-      <div className="d-flex">
-        <div className="me-2 rounded-5 overflow-hidden ">
-          <img src="https://via.placeholder.com/50" className="w-100" />
+    <div className="d-flex g-0 justify-content-between  p-2 align-items-center bg-c-grey my-2 mx-1 rounded-2 bg-c-grey-hover">
+      <div className="col-10 row g-0 gap-2">
+        <div className="me-2 col-3">
+          <img
+            src="https://via.placeholder.com/50"
+            className="w-100 rounded-circle"
+          />
         </div>
-        <div className="col-6">
+        <div className="col-8">
           <p className="mb-0">{name}</p>
           <small>@{name}</small>
         </div>
       </div>
       <div>
-        <button className="btn">
+        <button className="btn col-2">
           <FaUserPlus className="fs-2 text-grey-lite text-grey-lite-hover" />
         </button>
       </div>
