bca55328ca2292eb3d40f83a237079554fd20aee
Mahmoud Deghedy <mahmuddeghady@gmail.com>
2022-11-06 20:21:02 +0200

    Recommended updated

--- src/components/ChallengerShortcut/ChallengerShortcut.jsx
+++ src/components/ChallengerShortcut/ChallengerShortcut.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { FaUserPlus } from "react-icons/fa";
 
-const ChallengerShortcut = () => {
+const ChallengerShortcut = ({ name, photoURL }) => {
   return (
     <div className="d-flex justify-content-between border-top p-2 align-items-center">
       <div className="d-flex">
         <div className="me-2 rounded-5 overflow-hidden">
-          <img src="https://via.placeholder.com/50" className="w-100" />
+          <img src={photoURL} className="w-100" />
         </div>
         <div>
-          <p className="mb-0">Emy organda</p>
-          <small>@emyorganda</small>
+          <p className="mb-0">{name}</p>
+          <small>@{name}</small>
         </div>
       </div>
       <div>
--- src/components/RecommendedChallengers/RecommendedChallengers.jsx
+++ src/components/RecommendedChallengers/RecommendedChallengers.jsx
@@ -1,15 +1,55 @@
 import React from "react";
 import ChallengerShortcut from "../ChallengerShortcut/ChallengerShortcut";
 import { Link } from "react-router-dom";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { FirebaseContext } from "./../../context/FirebaseContext";
+import { currentContext } from "../../context/CurrentUser";
+
+import { useContext, useEffect, useState } from "react";
 
 const RecommendedChallengers = () => {
+  const { userCollection } = useContext(FirebaseContext);
+  const [users] = useCollectionData(userCollection);
+  const { currentUser } = useContext(currentContext);
+  const [recommendedUsersstate, setRecommendedUsers] = useState([]);
+
+  // console.log(users);
+  // console.log(currentUser);
+
+  const interestsVals = [];
+
+  useEffect(() => {
+    if (currentUser) {
+      currentUser[0]?.interests.forEach((interest) =>
+        interestsVals.push(interest.value)
+      );
+    }
+
+    // console.log(interestsVals);
+  }, [currentUser]);
+
+  useEffect(() => {
+    const recommendedUsers = [];
+    users?.forEach((usr) => {
+      usr.interests?.forEach((interest) => {
+        if (interestsVals.includes(interest.value)) {
+          recommendedUsers.push(usr);
+        }
+      });
+
+      setRecommendedUsers(recommendedUsers);
+    });
+
+    console.log("RECOMMENDEDDDDDD", recommendedUsers);
+  }, [users]);
+
   return (
     <div>
       <div className="border-bottom mb-2 text-white">
         <h5>Recommended challengers</h5>
-        <ChallengerShortcut />
-        <ChallengerShortcut />
-        <ChallengerShortcut />
+        {recommendedUsersstate.map((usr) => {
+          return <ChallengerShortcut name={usr.name} photoURL={usr.photoURL} />;
+        })}
       </div>
       <div className="p-2">
         <Link to="/" className="text-white">
