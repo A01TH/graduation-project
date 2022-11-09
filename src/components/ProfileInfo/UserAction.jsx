b412888817fb7887ebeb6f47347d2de2d75f1de7
Mostafa Khafaji <mostafakhafaji2050@gmail.com>
2022-11-09 22:11:34 +0200

    edit friend req feat

--- src/components/ProfileInfo/UserAction.jsx
+++ src/components/ProfileInfo/UserAction.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useContext } from "react";
 import {
+  AiFillClockCircle,
   AiOutlineMessage,
   AiOutlineStop,
   AiOutlineUserAdd,
@@ -8,24 +9,28 @@ import {
 import Toast from "../../UI/Toast/Toast";
 import { currentContext } from "../../context/CurrentUser";
 import { FirebaseContext } from "../../context/FirebaseContext";
-
+import { BsFillPersonCheckFill } from "react-icons/bs";
+import { MdPending } from "react-icons/md";
+import { CgSandClock } from "react-icons/cg";
 const UserAction = ({ user }) => {
   const { currentUser, updateCurrentUser, friends } =
     useContext(currentContext);
   const { userCollection } = useContext(FirebaseContext);
   const [sentReq, setSentReq] = useState(currentUser[0].sentRequests);
   const [recReq, setrecReq] = useState(user.receivedRequests);
-
+  const [isFriend, setIsFriend] = useState(false);
   const [clicked, setClicked] = useState(false);
   const handleReport = () => {
     console.log("Report");
   };
-  const handleChat = () => {
-    console.log("Chat");
-  };
+
   useEffect(() => {
     if (clicked) {
-      updateCurrentUser("sentRequests", sentReq, "Friend has been added");
+      updateCurrentUser(
+        "sentRequests",
+        sentReq,
+        "Friend Request has been sent!"
+      );
 
       userCollection
         .doc(user.uid)
@@ -43,6 +48,18 @@ const UserAction = ({ user }) => {
     }
   }, [sentReq]);
 
+  useEffect(() => {
+    if (currentUser) {
+      if (currentUser[0].friends.includes(user.uid)) {
+        console.log("found");
+        setIsFriend(true);
+      }
+      if (currentUser[0].sentRequests.includes(user.uid)) {
+        setClicked(true);
+      }
+    }
+  }, [currentUser[0].friends, currentUser[0].sentRequests]);
+
   const handleAddFriend = () => {
     setSentReq([...sentReq, user.uid]);
     setrecReq([...recReq, currentUser[0].uid]);
@@ -54,24 +71,28 @@ const UserAction = ({ user }) => {
         <button className="icon-btn text-secondary me-3 h4">
           <AiOutlineStop onClick={handleReport} />
         </button>
-        {friends.includes(user.uid) ? (
-          user.receivedRequests.includes(
-            currentUser[0].uid ? (
-              <div>sent</div>
+
+        {!isFriend ? (
+          <>
+            {clicked ? (
+              <i disabled className=" text-primary me-3 h4">
+                <MdPending />
+              </i>
             ) : (
-              <button className="icon-btn text-secondary me-3 h4">
-                <AiOutlineMessage onClick={handleChat} />
+              <button
+                className="icon-btn text-secondary me-3 h4"
+                onClick={() => handleAddFriend()}
+              >
+                <AiOutlineUserAdd />
               </button>
-            )
-          )
+            )}
+          </>
         ) : (
-          <button
-            className="icon-btn text-secondary me-3 h4"
-            onClick={() => handleAddFriend()}
-          >
-            <AiOutlineUserAdd />
-          </button>
+          <i className=" text-primary me-3 h4" disabled>
+            <BsFillPersonCheckFill />
+          </i>
         )}
+
         <Toast />
       </>
     </div>
