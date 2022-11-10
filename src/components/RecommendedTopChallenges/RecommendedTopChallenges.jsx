55b34231efcfb1957fca4fdca884131a569115bf
Mahmoud Deghedy <mahmuddeghady@gmail.com>
2022-11-10 02:26:16 +0200

    top challenge added / home

--- src/components/RecommendedTopChallenges/RecommendedTopChallenges.jsx
+++ src/components/RecommendedTopChallenges/RecommendedTopChallenges.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import ChallengerShortcut from "../ChallengerShortcut/ChallengerShortcut";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { useContext, useState, useEffect } from "react";
+import { FirebaseContext } from "../../context/FirebaseContext";
+import ChallengeCard from "../ChallengeCard/ChallengeCard";
 
 const RecommendedTopChallenges = () => {
+  const { challengeCollection } = useContext(FirebaseContext);
+  const [challenges] = useCollectionData(challengeCollection);
+  const [topChallenge, setTopChallenge] = useState([]);
+
+  useEffect(() => {
+    if (challenges) {
+      challenges.sort((a, b) => b.postLikes.length - a.postLikes.length);
+      setTopChallenge(challenges[0]);
+    }
+  }, [challenges]);
+
   return (
     <div className=" card  py-3 pb-2 text-white bg-body border-primary text-center">
       <h5 className="text-center mb-3 border-grey-lite pb-2 mx-3">
-        Top Challenges
+        Top Challenge
       </h5>
-      <ChallengerShortcut />
 
-      <div className="d-flex justify-content-around  p-2"></div>
+      <ChallengeCard post={topChallenge} />
     </div>
   );
 };
