bcae121b8885cf111d970bd85e38638c7d86f170
Mostafa Khafaji <mostafakhafaji2050@gmail.com>
2022-11-11 19:52:40 +0200

    edit styles

--- src/components/ProtectedRoutes/LoggedUser.js
+++ src/components/ProtectedRoutes/LoggedUser.js
@@ -4,6 +4,7 @@ import { currentContext } from "../../context/CurrentUser";
 
 export const LoggedUser = ({ children }) => {
   const { userData } = useContext(currentContext);
+
   if (userData) {
     return <Navigate to="/home"></Navigate>;
   }
